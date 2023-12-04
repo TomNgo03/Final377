662ff60142a372761914d661060965570c27f245
thpandaz <shaun.trinhha@gmail.com>
2023-12-04 18:59:38 -0500

    test

--- src/tsh.rs
+++ src/tsh.rs
@@ -1,50 +1,45 @@
-use std::process::{Command, exit};
-use core::str::from_utf8;
+use std::process::{Command, ExitStatus, Stdio};
+use std::io::{self, Read};
 
 pub struct SimpleShell;
 
 impl SimpleShell {
     pub fn new() -> SimpleShell {
-        return SimpleShell
+        SimpleShell
     }
 
     pub fn parse_command(&self, cmd: &str) -> Vec<String> {
-        return cmd.split_whitespace().map(String::from).collect()
+        cmd.split_whitespace().map(String::from).collect()
     }
 
-    pub fn exec_command(&self, argv: &[String]) {
+    pub fn exec_command(&self, argv: &[String]) -> io::Result<String> {
         match &argv[0][..] {
             "cd" => {
                 if argv.len() > 1 {
-                    match std::env::set_current_dir(&argv[1][..]) {
-                        Ok(_) => print!("{}", std::env::current_dir().unwrap().display()),
-                        Err(e) => eprintln!("cd failed: {}", e),
+                    match std::env::set_current_dir(&argv[1]) {
+                        Ok(_) => Ok(std::env::current_dir()?.display().to_string()),
+                        Err(e) => Err(e),
                     }
+                } else {
+                    Err(io::Error::new(io::ErrorKind::Other, "No directory specified"))
                 }
             },
             _ => {
-                match Command::new(&argv[0])
+                let output = Command::new(&argv[0])
                     .args(&argv[1..])
-                    .spawn()
-                {
-                    Ok(mut child) => {
-                        match child.wait() {
-                            Ok(status) => {
-                                if !status.success() {
-                                    exit(1);
-                                }
-                            }
-                            Err(e) => eprintln!("Failed to wait for command: {}", e),
-                        }
-                    }
-                    Err(e) => eprintln!("Failed to execute command: {}", e),
+                    .output()?;
+
+                if output.status.success() {
+                    Ok(String::from_utf8_lossy(&output.stdout).trim().to_string())
+                } else {
+                    Err(io::Error::new(io::ErrorKind::Other, "Command execution failed"))
                 }
-            },  
+            },
         }
     }
 
     pub fn is_quit(&self, cmd: &str) -> bool {
-        return cmd == "quit"
+        cmd == "quit"
     }
 }
 
@@ -53,47 +48,33 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_is_quit(){
-        let mut shell = SimpleShell::new();
-        assert_eq!(shell.is_quit("quit"), true);
-        assert_eq!(shell.is_quit("ls"), false);
+    fn test_is_quit() {
+        let shell = SimpleShell::new();
+        assert!(shell.is_quit("quit"));
+        assert!(!shell.is_quit("ls"));
     }
-    
+
     #[test]
     fn test_parse_command() {
         let shell = SimpleShell::new();
 
         let argv = shell.parse_command("echo Hello, World!");
         assert_eq!(argv, vec!["echo", "Hello,", "World!"]);
-
-        let argv = shell.parse_command("echo -n Hello, World!");
-        assert_eq!(argv, vec!["echo", "-n", "Hello,", "World!"]);
     }
 
     #[test]
-    fn test_exec_command() {
+    fn test_exec_command_echo() {
         let shell = SimpleShell::new();
-    
-        // Test command execution
-        let cmd = shell.parse_command("echo Hello, World!");
-        let output = shell.exec_command(&cmd).unwrap();
-        assert_eq!(from_utf8(&output.stdout).unwrap().trim(), "Hello, World!");
-        assert!(output.status.success());
-    
-        // Test command with arguments
-        let cmd = shell.parse_command("echo -n Hello, World!");
-        let output = shell.exec_command(&cmd).unwrap();
-        assert_eq!(from_utf8(&output.stdout).unwrap(), "Hello, World!");
-        assert!(output.status.success());
+
+        let argv = shell.parse_command("echo Hello, World!");
+        assert_eq!(shell.exec_command(&argv).unwrap(), "Hello, World!");
     }
 
     #[test]
-    fn test_cd_command() {
+    fn test_exec_command_cd() {
         let shell = SimpleShell::new();
         let argv = shell.parse_command("cd /tmp");
-        let output = shell.exec_command(&argv).unwrap();
-        assert!(output.status.success());
-        assert_eq!(from_utf8(&output.stdout).unwrap().trim(), "/tmp");
+        shell.exec_command(&argv).unwrap();
+        assert_eq!(std::env::current_dir().unwrap().display().to_string(), "/tmp");
     }
-
 }
